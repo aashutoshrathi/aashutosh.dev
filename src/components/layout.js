e2db04b3164824a000457436ec5f3bee66e21b0b
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-01-27 00:19:24 +0530

    :construction_worker: Add banner for beta and add async to scripts

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,8 +18,18 @@ const Layout = ({ children }) => {
           rel="stylesheet"
           href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"
         />
-        <script src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js"></script>
+        <script
+          async
+          src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js"
+        ></script>
       </Helmet>
+      <div className="uk-alert-primary" uk-alert>
+        <p className="uk-text-center">
+          This is site is currently in beta, if you have any suggestions ping me
+          at <a href="emailto:me@aashutosh.dev">me@aashutosh.dev</a>. Thanks for
+          visiting 😬. Simply scroll a bit to ignore this.
+        </p>
+      </div>
       <div className="site">
         <Header />
         <div className={"site-content"}>{children}</div>
