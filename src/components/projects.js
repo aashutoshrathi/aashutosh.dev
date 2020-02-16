fa2bb5a8972033f99c925f6a9ed638ec67f8dbba
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-02-16 21:31:55 +0530

    feat: Use stale while revalidate

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react"
 import Card from "./card.js"
 
+const CACHE = {}
+const PROJECT_URL = process.env.GATSBY_API_URI
+
 const Projects = () => {
   const [projects, setProjects] = useState([])
 
   const getProjects = () => {
-    fetch(process.env.GATSBY_API_URI)
+    fetch(PROJECT_URL)
       .then(res => res.json())
       .then(data => {
         data = data.filter(repo => !repo.fork && repo.stargazers_count > 1)
         data.sort((a, b) => {
           return a.stargazers_count > b.stargazers_count ? -1 : 1
         })
+        CACHE[PROJECT_URL] = data
         setProjects(data)
       })
       .catch(err => {
@@ -20,6 +24,9 @@ const Projects = () => {
   }
 
   useEffect(() => {
+    if (CACHE[PROJECT_URL] !== undefined) {
+      setProjects(CACHE[PROJECT_URL])
+    }
     getProjects()
   }, [])
 
