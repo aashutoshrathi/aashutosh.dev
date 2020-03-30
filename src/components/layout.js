44d9c6a98a8a402ff2e8e4652f5c898f733dbf8b
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-03-30 12:02:33 +0530

    move calendar stuff to work

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Helmet from "react-helmet"
 
 import Header from "./header"
 import Footer from "./footer"
@@ -9,12 +8,6 @@ import "./layout.css"
 const Layout = ({ children }) => {
   return (
     <>
-      <Helmet>
-        <script
-          async
-          src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js"
-        ></script>
-      </Helmet>
       <div className="site">
         <Header />
         <main className={"site-content"}>{children}</main>
