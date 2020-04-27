75bf0906d3269f22242dbf1df1c7b0785e4e3ea0
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-04-27 23:02:14 +0530

    fallback to initial approach (#13)
    * fallback to initial approach
    
    * Remove unsafe-inline
    
    * Add hashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
+const cspDirectives = [
+  "script-src 'self' *.cloudfront.net unpkg.com 'sha256-egpbluqkD8NT0bY3bWy7raM9tRIMkfUWboq0Y8KqsFk=' 'sha256-RO/fsOOG1Pjr/GU/rMC3PRDUlAZbJbReLigQo4Ed+cw='",
+  "style-src 'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com",
+  "img-src 'self' data: *.githubusercontent.com *.githubassets.com",
+  "font-src 'self' data: fonts.googleapis.com fonts.gstatic.com",
+  "worker-src 'self' blob: data:",
+  "connect-src 'self' api.aashutosh.dev urlreq.appspot.com",
+  "object-src 'none'",
+  "require-trusted-types-for 'script'",
+]
+
 module.exports = {
   siteMetadata: {
     title: `Aashutosh Rathi | Blog`,
@@ -38,28 +49,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        mergeScriptHashes: false,
-        mergeStyleHashes: false,
-        directives: {
-          "script-src": `'self' 'unsafe-inline' *.cloudfront.net unpkg.com`,
-          "style-src": `'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com`,
-          "img-src": `'self' data: *.githubusercontent.com *.githubassets.com`,
-          "font-src": `'self' data: fonts.googleapis.com fonts.gstatic.com`,
-          "worker-src": `'self' blob: data:`,
-          "connect-src": `'self' api.aashutosh.dev urlreq.appspot.com`,
-          "object-src": `'none'`,
-          "require-trusted-types-for": `'script'`,
-        },
-      },
-    },
-    {
       resolve: "gatsby-plugin-netlify",
       options: {
         headers: {
           "/*": [
-            "Content-Security-Policy: __REPLACE_ME__",
+            `Content-Security-Policy: ${cspDirectives.join(";")}`,
             "X-Frame-Options: DENY",
             "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload",
             "Upgrade-Insecure-Requests: 1",
