b5fa08068765e80baa0af37db8b29d75abefee69
Aashutosh Rathi <aashutoshrathi@gmail.com>
2020-10-30 20:34:44 +0530

    fix: Remove Git Calendar until it is fixed

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,32 +1,30 @@
-import GitHubCalendar from "github-calendar"
-import React, { useEffect } from "react"
-import Helmet from "react-helmet"
+import React from "react"
 import Layout from "../components/layout"
 import Projects from "../components/projects"
 import SEO from "../components/seo"
 import "./work.css"
 
 const IndexPage = () => {
-  useEffect(() => {
-    GitHubCalendar(".calendar", "aashutoshrathi", { responsive: true })
-  }, [])
+  // useEffect(() => {
+  //   GitHubCalendar(".calendar", "aashutoshrathi", { responsive: true })
+  // }, [])
 
   return (
     <Layout>
-      <Helmet>
+      {/* <Helmet>
         <script
           async
           src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js"
         ></script>
-      </Helmet>
+      </Helmet> */}
       <SEO title="Work" />
-      <h3 className={["text-center", "padding-small", "heading"].join(" ")}>
+      {/* <h3 className={["text-center", "padding-small", "heading"].join(" ")}>
         My GitHub Calendar{" "}
         <span role="img" aria-label="calendar">
           🗓
         </span>{" "}
       </h3>
-      <div className="calendar">Loading the data just for you.</div>
+      <div className="calendar">Loading the data just for you.</div> */}
 
       <h3
         className={[
