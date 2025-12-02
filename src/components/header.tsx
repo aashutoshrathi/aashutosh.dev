e62d4dd7462c6abd905e89a7bd292e1fa1f6be3c
Aashutosh Rathi <aashutoshrathi@gmail.com>
2025-12-02 20:14:04 +0530

    ✨ feat: update active link styles in header for improved visibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,23 +5,23 @@ import { OutboundLink } from "gatsby-plugin-google-gtag"
 const Header: React.FC = () => (
   <header className="py-4">
     <nav className="p-[1%] flex flex-wrap flex-row justify-between items-center px-4 sm:px-6 lg:px-8 max-[500px]:flex-col">
-      <Link className="text-center text-[1.3rem] font-bold font-mono p-2" to="/" activeClassName="font-bold">
+      <Link className="text-center text-[1.3rem] font-bold font-mono p-2" to="/" activeClassName="border-b-2 border-dashed border-link">
         {"aashutosh.dev"}
       </Link>
       <nav className="flex flex-row justify-between flex-wrap mx-[0.7rem]">
-        <Link to="/work/" className="mx-[0.7rem]" activeClassName="font-bold">
+        <Link to="/work/" className="mx-[0.7rem]" activeClassName="border-b-2 border-dashed border-link">
           {"Work"}
         </Link>
-        <Link to="/timeline/" className="mx-[0.7rem]" activeClassName="font-bold">
+        <Link to="/timeline/" className="mx-[0.7rem]" activeClassName="border-b-2 border-dashed border-link">
           {"Timeline"}
         </Link>
-        <Link to="/uses/" className="mx-[0.7rem]" activeClassName="font-bold">
+        <Link to="/uses/" className="mx-[0.7rem]" activeClassName="border-b-2 border-dashed border-link">
           {"Uses"}
         </Link>
-        <Link to="/blog/" className="mx-[0.7rem]" activeClassName="font-bold">
+        <Link to="/blog/" className="mx-[0.7rem]" activeClassName="border-b-2 border-dashed border-link">
           {"Blog"}
         </Link>
-        <Link to="/resume/" className="mx-[0.7rem]" activeClassName="font-bold">
+        <Link to="/resume/" className="mx-[0.7rem]" activeClassName="border-b-2 border-dashed border-link">
           {"Résumé"}
         </Link>
       </nav>
